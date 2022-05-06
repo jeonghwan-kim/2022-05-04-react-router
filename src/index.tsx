754123c3786f2d7136aba0d703cbaacfd2e144f1
김정환 <ej88ej@gmail.com>
2022-05-06 16:31:28 +0900

    정리

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./MyRouter1";
+// import App from "./MyRouter2";
+// import App from "./MyRouter3";
+// import App from "./MyRouter4";
+// import App from "./MyRouter5";
+// import App from "./MyRouter6";
+import App from "./MyRouterHook";
+// import App from "./MyRouter";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
